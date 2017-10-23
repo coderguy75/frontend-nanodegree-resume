972e9366a1da5f74f0eb53e62af14050ba8ed481
Bluesky <32860118+coderguy75@user.noreply.github.com>
2017-10-23 10:34:25 -0700

    Initial project

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,91 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var bio = { //define variables for bio
+    "name": "Azam Khan",
+    "role": "Web Developer",
+    "contacts": {
+        "mobile": "(425)786-7377",
+        "email": "mk9377@att.com",
+        "github": "32860118+coderguy75@user.noreply.github.com",
+        "location": "Redmond, Washington"
+    },
+    "welcomeMessage": "Welcome to my interactive resume!",
+    "skills": ["Network Planning", "Highly Motivated",
+        "Awesome Person", "Collaborator", "People Person"
+    ],
+    "biopic": "images/me.jpg",
+};
+var work = { //define variables for work
+    "jobs": [{
+        "employer": "AT&T",
+        "title": "Professional Network Planning Engineer+",
+        "dates": "06/2010 - Present",
+        "location": "Redmond, Washington",
+        "description": "Plan and Design Microwave backhaul transport Network for ETTCS, UMTS and LTE technology for AT&T Mobility sites. "
+    }, {
+        "employer": "Alltel Communicaton Inc.",
+        "title": "Microwave Engineer -III",
+        "dates": "05/2008 - 06/2010",
+        "location": "Bellevue, Washington",
+        "description": "Plan and Design Microwave backhaul transport Network for Alltel Cell sites. "
+    }, {
+        "employer": "T-Mobile USA",
+        "title": "Microwave Engineer",
+        "dates": "06/2007 - 5/2008",
+        "location": "Bothell, Washington",
+        "description": "Plan and Design Microwave backhaul transport Network for for T-Mobile Cell sites."
+    }, {
+        "employer": "Sun Telecom Group",
+        "title": "Network Engineer",
+        "dates": "01/2006 - 04/2007",
+        "location": "Montreal, Quebec, Canada",
+        "description": "Installed, operated and maintained VOIP Telecommunication Network system serving wholesale customers."
+    }, {
+        "employer": "Bangladesh T&T Board",
+        "title": "Telecommunication Engineer",
+        "dates": "04/1994 - 09/2002",
+        "location": "Dhaka, Bangladesh",
+        "description": "Responsible for Planning, design, testing, and maintaining various types of voice and carrier equipment including fiber transport systems. "
+    }]
+};
+var education = { //define variables for education
+    "schools": [{
+        "name": "Concordia University",
+        "location": "Montreal, Quebec, Canada",
+        "degree": "Masters of Elecrtical Engineering",
+        "majors": ["Wireless Telecommunication", "Power Electronics"],
+        "dates": "2003-2005",
+        "URL": "https://www.concordia.ca/",
+    }, {
+        "name": "Khulna University of Engineering & Technology",
+        "location": "Khulna, Bangladesh",
+        "degree": "Bachelor of Science in Engineering",
+        "majors": ["Electrical Engineering", "Electronics Engineering"],
+        "dates": "1985-1989",
+        "URL": "http://www.kuet.ac.bd/",
+    }],
+
+    "onlineCourses": [{
+
+        "title": "Frontend Web Design",
+        "school": "Udacity",
+        "dates": "April 2017 - Present",
+        "url": "https://www.udacity.com",
+
+    }, {
+        "title": "Software Defined Network",
+        "school": "T-University",
+        "dates": "January 2015 - July 2016",
+        "url": "http://att.jobs/doing-great-things/life-at-att/learning-development/att-university",
+    }],
+};
+
+var projects = { //define variables for projects
+    "projects": [{
+        "title": "Project Zodiac",
+        "dates": "June 2010 to July 2011",
+        "description": "Design of overlay Microwave network for Alltel acquisition.",
+        "images": ["images/xxxx.jpg", "images/xx.jpg", "images/ssss.jpg"]
+    }]
+};
